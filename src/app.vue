ed2818906e48ca39bfc17cdc69616146b1267011
Jamie Syme <jamie.d.syme@gmail.com>
2018-10-02 18:07:08 -0700

    Added command bar.

---
+++ src/app.vue
@@ -0,0 +1,43 @@
+<template>
+	<cmd-bar></cmd-bar>
+</template>
+
+<script>
+	import CmdBar from './widgets/cmd-bar';
+
+	export default {
+		components: {
+			CmdBar,
+		},
+
+		data() {
+			return {};
+		},
+	};
+</script>
+
+<style lang="less">
+	*,
+	*::after,
+	*::before {
+		box-sizing: inherit;
+	}
+
+	html {
+		box-sizing: border-box;
+		font-size: 62.5%;
+		height: 100%;
+	}
+
+	body {
+		font-family: sans-serifx;
+		font-size: 1.6em;
+		letter-spacing: 0.01em;
+		line-height: 1.6;
+		min-height: 100%;
+		margin: 0;
+		background-color: #565656;
+		//display: flex;
+		//flex-direction: column;
+	}
+</style>
---
+++ src/widgets/cmd-bar/index.vue
@@ -0,0 +1,50 @@
+<template>
+	<div class="cmd-bar">
+		<input
+			type="text"
+			placeholder="Enter a command...">
+	</div>
+</template>
+
+<script>
+</script>
+
+<style lang="less" scoped>
+	.cmd-bar {
+		padding: 2rem;
+		background-color: #404040;
+		box-shadow: 0 0 0.3rem #333;
+	}
+
+	input {
+		display: block;
+		width: 33%;
+		min-width: 20.0rem;
+		max-width: 60.0rem;
+		margin: 0 auto;
+
+		background-color: #bfbfbf;
+		border: none;
+		border-radius: 1.5rem;
+		box-shadow: 0 0 0.3rem #404040 inset;
+		color: #404040;
+		padding: 0.6rem 1.5rem;
+		font-size: 1.6rem;
+		transition: all 0.15s;
+		outline: 0;
+
+		&:focus {
+			background-color: #f4f4f4;
+			&::placeholder {
+				opacity: 0;
+			}
+		}
+
+		&::placeholder {
+			color: #505050;
+			font-style: italic;
+			opacity: 1;
+			transition: inherit;
+		}
+	}
+</style>
